749e1d03c0553425d1ec88601d44a98d98f68a77
Travis Carlson <travis.carlson@cs.umn.edu>
2017-10-12 13:32:10 -0500

    import injectable

--- grammars/edu.umn.cs.melt.exts.ableC.dimensionalAnalysis/abstractsyntax/Units.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.dimensionalAnalysis/abstractsyntax/Units.sv
@@ -4,6 +4,7 @@ imports edu:umn:cs:melt:ableC:abstractsyntax;
 imports edu:umn:cs:melt:ableC:abstractsyntax:env;
 imports silver:langutil;
 imports silver:langutil:pp;
+imports edu:umn:cs:melt:ableC:abstractsyntax:injectable;
 
 import edu:umn:cs:melt:ableC:abstractsyntax:overload as ovrld;
 
